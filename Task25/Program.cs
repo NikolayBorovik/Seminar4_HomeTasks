77d020035694102b0bcbc6bdb6a1fded3ed72afb
Nikolay Borovik <nik.Borovik@bk.ru>
2023-02-07 15:44:01 +1000

    Taks25 done

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 25. Цикл, кот принимает на вх два числа А и Б и возводит число А в натуральную степень Б.
+// 3,5 - 243(3 в 5 степени)
+
+Console.Clear();
+
+System.Console.WriteLine("Enter your numberA: ");
+int numberA = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Enter your numberB: ");
+int numberB = int.Parse(Console.ReadLine());
+
+int numAPowNumB (int numberA, int numberB)
+{
+    int pow = numberA;
+    for(int i = 1; i < numberB; i++)
+    {
+        pow = pow*numberA;
+    }
+    return pow;
+}
+
+if (numberB < 0)
+{
+    System.Console.WriteLine("Your number < 0.");
+}
+else if (numberB == 0)
+{
+    System.Console.WriteLine("NumberA raised to the power of the NumberB is 1");
+}
+else
+{
+    System.Console.WriteLine($"NumberA raised to the power of the NumberB is {numAPowNumB(numberA, numberB)}");
+}
