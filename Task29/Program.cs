ef0602dd8708e90b2eee55068524a3fd5392415f
Nikolay Borovik <nik.Borovik@bk.ru>
2023-02-07 17:05:02 +1000

    Taks29 done

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 29. Пр-ма, кот задает массив из 8 эл-в и выводит их на экран.
+// 1,2,5,7,19 - [1, 2, 5, 7, 19]
+
+Console.Clear();
+
+int[] array = new int[8];
+
+void FillArray(int[] array)
+{
+    int length = array.Length;
+    for(int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(1, 10);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    int length = array.Length;
+    for(int i = 0; i < length; i++)
+    {
+        System.Console.Write(array[i]);
+    }
+}
+
+FillArray(array);
+PrintArray(array);
